5b1e136474334c42e334086fb8dfcc4d024494ba
Sukhpreet Sidhu <sukhpreet8247@gmail.com>
2024-08-07 22:45:43 -0700

    Renamed spotimages model to spotimage

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -3,17 +3,17 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class SpotImages extends Model {
+  class SpotImage extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      SpotImages.hasMany(models.Spot, { foreignKey:'spotId' })
+      SpotImage.hasMany(models.Spot, { foreignKey:'spotId' })
     }
   }
-  SpotImages.init({
+  SpotImage.init({
     spotId: {
       type:DataTypes.INTEGER,
       allowNull:false
@@ -31,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    modelName: 'SpotImages',
+    modelName: 'SpotImage',
   });
-  return SpotImages;
+  return SpotImage;
 };
